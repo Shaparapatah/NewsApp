7ca9b6f28e20deaea1f46609002bf857c3e11b5f
shaparapatah <mm.fedorchenko@gmail.com>
2022-10-04 14:01:05 +0200

    Create Data classes, retrofit service and app module

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "9165b4bf40624f88af4e2e17e1260a44"
+    }
+}
