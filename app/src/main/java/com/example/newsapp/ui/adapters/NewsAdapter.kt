deae10fa5b71497a7a5fc6785c5d33e219b96182
shaparapatah <mm.fedorchenko@gmail.com>
2022-10-04 22:51:32 +0200

    Create recyclerView, adapter

---
+++ app/src/main/java/com/example/newsapp/ui/adapters/NewsAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.newsapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+import com.example.newsapp.model.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+
+    inner class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    private val callback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, callback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        return NewsViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this)
+                .load(article.urlToImage)
+                .into(findViewById(R.id.recycler_item_iv))
+            findViewById<ImageView>(R.id.recycler_item_iv).clipToOutline = true
+            findViewById<TextView>(R.id.title_tv).text = article.title
+            findViewById<TextView>(R.id.tv_date).text = article.publishedAt
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
