9535420ec1827ab5f5669b79b09077faa955e840
shaparapatah <mm.fedorchenko@gmail.com>
2022-10-11 22:00:28 +0200

    Change dimension on recycler image

--- app/src/main/java/com/example/newsapp/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/favorite/FavoriteFragment.kt
@@ -1,12 +1,10 @@
 package com.example.newsapp.ui.favorite
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newsapp.R
-import com.example.newsapp.databinding.FragmentDetailsBinding
+import androidx.fragment.app.Fragment
 import com.example.newsapp.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
